a01c32c0e585db1f51660c0df2312f0a3f12f360
Li Haoquan <64543157+lihaoquan@users.noreply.github.com>
2023-02-13 21:31:48 +0800

    ReactJS Front End UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
 import './App.css';
 
 function App() {
+
+  const [form, setForm] = useState({
+    url: '',
+    expires_on: null
+  })
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault()
+
+    axios.post('http://localhost:3000/generate', form)
+      .then(function (response) {
+        
+      }).catch(function (error) {
+
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Shorten URL</h1>
+        <p>Enter a URL to be shortened.</p>
+        <p><i>(It is optional to set an expiry date.)</i></p>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>URL</label>
+            <input type="url" placeholder="Enter URL" required onChange={(e) => { setForm({ ...form, url: e.target.value }) }} />
+          </div>
+          <div className="form-group">
+            <label>Expiry</label>
+            <input type="datetime-local" onChange={(e) => { setForm({ ...form, expires_on: e.target.value }) }} />
+          </div>
+          <button>Generate</button>
+        </form>
+        <p>Created by <a href="https://lihaoquan.com/">Li Haoquan</a></p>
       </header>
     </div>
   );

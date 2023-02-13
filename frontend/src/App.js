7bcb30cce7c6ca897e2e5ee40ae167d429a72aec
Li Haoquan <64543157+lihaoquan@users.noreply.github.com>
2023-02-14 00:42:23 +0800

    Fix Link Bug in Production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ function App() {
 
       setButtonDisabled(true)
 
-      axios.post(process.env.REACT_APP_RUNTIME_ENV == "PROD" ? process.env.REACT_APP_API_ENDPOINT_PROD : process.env.REACT_APP_API_ENDPOINT_DEV + 'generate', form)
+      axios.post(process.env.REACT_APP_RUNTIME_ENV == "PROD" ? process.env.REACT_APP_API_ENDPOINT_PROD + 'generate' : process.env.REACT_APP_API_ENDPOINT_DEV + 'generate', form)
         .then(function (response) {
           if (response.status == 200) {
             setShortURL(response.data.data)
